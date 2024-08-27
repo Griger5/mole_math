c0ea87c8171c38c07d8a442a809e6c91d128f838
Graca <griger04@gmail.com>
2024-08-27 21:32:25 +0200

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -5,23 +5,30 @@ compile() {
     make
 }
 
-copy_to_usr_lib() {
+install_lib() {
+    cp -r include/mole_math /usr/local/include/ 
+    compile
     cp lib/libmolemath.so /usr/local/lib/ 
-	cp -r include/mole_math /usr/local/include/ 
+    ldconfig
 }
 
 case $1 in
+    install)
+        install_lib
+        exit 0
+        ;;
     uninstall)
         rm /usr/local/lib/libmolemath.so
         rm -r /usr/local/include/mole_math/
         exit 0
         ;;
     *)
+        if [ -z "$1" ]
+        then
+            install_lib
+            exit 0
+        fi
         echo "Error: Unrecognized command."
         exit 0
         ;;
 esac
-
-compile
-copy_to_usr_lib
-ldconfig
